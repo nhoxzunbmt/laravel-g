6e5a70a6df58ad45971c4d9d2cdf380a79399700
Hawkart <hawkart@rambler.ru>
2017-12-04 18:06:59 +0300

    Google api contacts bugfix 3

--- app/User.php
+++ app/User.php
@@ -36,7 +36,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
     protected $fillable = [
         'name', 'email', 'password', 'nickname', 'phone', 'last_name', 'second_name', 'avatar', 'min_sponsor_fee', 
         'overlay', 'description', 'type', 'country_id', 'confirmation_code', 'team_id', 'game_id', 'streams', 'free_player', 'schedule',
-        'confirmed', 'active', 'timezone', 'ip', 'city', 'geo'
+        'confirmed', 'active', 'timezone', 'ip', 'city', 'geo', 'contacts'
     ];
 
     /**
@@ -45,7 +45,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'api_token', 'confirmation_code', 'ip', 'geo'
+        'password', 'remember_token', 'api_token', 'confirmation_code', 'ip', 'geo', 'contacts'
     ];
     
     
@@ -57,7 +57,8 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
     protected $casts = [
         'streams' => 'array',
         'schedule' => 'array',
-        'geo' => 'array'
+        'geo' => 'array',
+        'contacts' => 'array'
     ];
     
     protected $searchableColumns = [
